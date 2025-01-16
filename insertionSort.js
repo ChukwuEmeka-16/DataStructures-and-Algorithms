402edcb19081cf901ddd5417893407c56fa7e6d4
eli <jomabulok@gmail.com>
2025-01-16 08:45:37 -0800

    attempted an implementation of insertion sort(does not work well)

---
+++ insertionSort.js
@@ -0,0 +1,29 @@
+let listNums = [8,17,2,23,3,48,51,600,71,12,8,5,9,11,10,6,250,45,34,2,16,22,18,55,1]
+
+
+// not working, ill try again tomorrow
+
+const insertionSort = (array)=>{
+    let temp_val_index = 1;
+    for(let o = 0; o < array.length; o++) {
+        console.log(array);
+        
+        for (let i = temp_val_index ; i !=0; i--) {
+            
+            if (array[temp_val_index] < array[i-1]) {
+                let numOne = array[i];
+                let numTwo = array[i-1];
+                array[i] = numTwo;
+                array[i-1] =numOne;
+            }
+            else{
+                break;
+            }
+            
+        }
+
+        temp_val_index++;
+    }
+}
+
+insertionSort(listNums)
