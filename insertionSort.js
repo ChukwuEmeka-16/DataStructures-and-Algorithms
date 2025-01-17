3011c4b3fcd31cd284ab2b43dfc4b05ec9366e2a
eli <jomabulok@gmail.com>
2025-01-17 12:09:56 -0800

    test push on new branch

--- insertionSort.js
+++ insertionSort.js
@@ -1,6 +1,7 @@
 let listNums = [8,17,2,23,3,48,51,600,71,12,8,5,9,11,10,6,250,45,34,2,16,22,18,55,1]
 
 // update on test branch
+// test branch 
 // not working, ill try again tomorrow
 
 const insertionSort = (array)=>{
